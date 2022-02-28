135ddd3361d969d006804e6cf482aab07edaae0d
Moutz Dahhan <mdahhan4@outlook.com>
2022-02-28 04:39:13 -0500

    Created Projectiles

--- index.js
+++ index.js
@@ -48,7 +48,28 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 3
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const player = new Player()
+const projectiles = []
 const keys = {
     a: {
         pressed: false
@@ -66,6 +87,17 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0) {
+            setTimeout(() =>{
+                projectiles.splice(index, 1)
+            }, 0)
+            
+        } else {
+            projectile.update()
+        }
+        
+    })
     if (keys.a.pressed && player.position.x >=0) {
         player.velocity.x = -7
         player.rotation = -0.15
@@ -83,16 +115,27 @@ animate()
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'a':
-            console.log('left')
+            // console.log('left')
             
             keys.a.pressed = true
             break
             case 'd':
-            console.log('right')
+            // console.log('right')
             keys.d.pressed = true
             break
             case ' ':
-            console.log('space')
+            // console.log('space')
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            }))
+            // console.log(projectiles)
             break
     }
 }) 
@@ -100,16 +143,16 @@ addEventListener('keydown', ({key}) => {
 addEventListener('keyup', ({key}) => {
     switch (key) {
         case 'a':
-            console.log('left')
+            // console.log('left')
             
             keys.a.pressed = false
             break
             case 'd':
-            console.log('right')
+            // console.log('right')
             keys.d.pressed = false
             break
             case ' ':
-            console.log('space')
+            // console.log('space')
             break
     }
 }) 
